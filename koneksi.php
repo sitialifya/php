1c862b0843e8918df5588cfc49279234bedf85d2
Wawan Sismadi <wawan@sismadi.com>
2026-05-07 00:23:46 +0700

    Update koneksi.php

--- koneksi.php
+++ koneksi.php
@@ -1,4 +1,9 @@
 <?php
+
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
+
 $host = "mysql.railway.internal";
 $user = "root";
 $pass = "LZstNchgUtUIzpLIpzjvtJVusCMZscPX"; // Sesuaikan dengan MYSQL_ROOT_PASSWORD di gambar
